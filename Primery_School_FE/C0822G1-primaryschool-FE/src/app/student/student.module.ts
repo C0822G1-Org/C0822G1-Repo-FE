76cccb7facce812d59bbcc04d89a3349dda17a45
Your Name <you@example.com>
2023-03-01 10:05:50 +0700

    up

--- Primery_School_FE/C0822G1-primaryschool-FE/src/app/student/student.module.ts
+++ Primery_School_FE/C0822G1-primaryschool-FE/src/app/student/student.module.ts
@@ -7,10 +7,11 @@ import { StudentCreateComponent } from './student-create/student-create.componen
 import { StudentUpdateComponent } from './student-update/student-update.component';
 import { StudentDetailComponent } from './student-detail/student-detail.component';
 import { StudentDeleteComponent } from './student-delete/student-delete.component';
+import { HomeroomClassComponent } from './homeroom-class/homeroom-class.component';
 
 
 @NgModule({
-  declarations: [StudentListComponent, StudentCreateComponent, StudentUpdateComponent, StudentDetailComponent, StudentDeleteComponent],
+  declarations: [StudentListComponent, StudentCreateComponent, StudentUpdateComponent, StudentDetailComponent, StudentDeleteComponent, HomeroomClassComponent],
   imports: [
     CommonModule,
     StudentRoutingModule
--- Primery_School_FE/C0822G1-primaryschool-FE/src/app/timetable/timetable.module.ts
+++ Primery_School_FE/C0822G1-primaryschool-FE/src/app/timetable/timetable.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { TimetableRoutingModule } from './timetable-routing.module';
 import { TimetableListComponent } from './timetable-list/timetable-list.component';
+import { TimetableTeacherComponent } from './timetable-teacher/timetable-teacher.component';
 
 
 @NgModule({
-  declarations: [TimetableListComponent],
+  declarations: [TimetableListComponent, TimetableTeacherComponent],
   imports: [
     CommonModule,
     TimetableRoutingModule

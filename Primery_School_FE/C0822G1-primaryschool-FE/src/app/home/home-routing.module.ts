f936546901a048adfad69fc2103fb3b097ef5ea8
TranVanSy2005 <tranvansy197@gmail.com>
2023-03-03 11:10:35 +0700

    SyTV update code for login logout

--- Primery_School_FE/C0822G1-primaryschool-FE/src/app/home/home-routing.module.ts
+++ Primery_School_FE/C0822G1-primaryschool-FE/src/app/home/home-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {BodyComponent} from "./body/body.component";
-import {AdminGuard} from '../authguard/admin.guard';
 
 const routes: Routes = [
-  {path: '', component: BodyComponent , canActivate: [AdminGuard]},
+  {path: '', component: BodyComponent},
 ];
 
 @NgModule({
